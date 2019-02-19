559878d91f444eb86130ca421db54d3cbcd8c1a1
timmoo <47295295+timmoo@users.noreply.github.com>
2019-02-19 21:44:02 +0300

    Update SQLQuery3-5.sql

--- SQLQuery3-5.sql
+++ SQLQuery3-5.sql
@@ -1,37 +1,19 @@
-/*
-Msg 15281, Level 16, State 1, Procedure master..xp_cmdshell, Line 1 [Batch Start Line 0]
-SQL Server blocked access to procedure 'sys.xp_cmdshell' of component 'xp_cmdshell' because this component is turned off as part of the security configuration for this server. 
-A system administrator can enable the use of 'xp_cmdshell' by using sp_configure. For more information about enabling 'xp_cmdshell', search for 'xp_cmdshell' in SQL Server Books Online.
-*/
-
--- To allow advanced options to be changed.  
-EXEC sp_configure 'show advanced options', 1;  
-GO  
--- To update the currently configured value for advanced options.  
-RECONFIGURE;  
-GO  
--- To enable the feature.  
-EXEC sp_configure 'xp_cmdshell', 1;  
-GO  
--- To update the currently configured value for this feature.  
-RECONFIGURE;  
-GO  
-
-SELECT @@SERVERNAME
-
---exec master..xp_cmdshell 'bcp "WorldWideImporters.Sales.InvoiceLines" out  "E:\SQL WWI\InvoiceLines.txt" -c -t, -S localhost\MSSQLServer01 -T -b 10000'
 
 
-exec master..xp_cmdshell 'bcp "[WideWorldImporters].Purchasing.Suppliers" out  "C:\SQL WWI\Suppliers.txt" -T -w -t, -S'
 
-BULK INSERT [WideWorldImporters].[Purchasing].[Suppliers]
+exec master..xp_cmdshell 'bcp "[WideWorldImporters].Purchasing.Suppliers" out  "C:\SQL WWI\Suppliers.txt" -T -w -t"_" -S'
+
+Select * into [WideWorldImporters].[Purchasing].[Suppliers_Copy] from [WideWorldImporters].Purchasing.Suppliers
+Truncate table [WideWorldImporters].[Purchasing].[Suppliers_Copy]
+BULK INSERT [WideWorldImporters].[Purchasing].[Suppliers_Copy]
 				   FROM "C:\SQL WWI\Suppliers.txt"
 				   WITH 
 					 (
 						BATCHSIZE = 1000, 
 						DATAFILETYPE = 'widechar',
-						FIELDTERMINATOR = '@eu&$1&',
+						FIELDTERMINATOR = '_',
 						ROWTERMINATOR ='\n',
 						KEEPNULLS,
-						TABLOCK        
+						TABLOCK       
+						 
 					  );
